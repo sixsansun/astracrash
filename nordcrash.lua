5a1f9d2c3552f56843e967e316f9c0a055641676
６００１ <sixsansun@protonmail.com>
2026-08-18 14:25:23 +0200

    Create nordcrash.lua

---
+++ nordcrash.lua
@@ -0,0 +1,12 @@
+local Event = game:GetService("Players").LocalPlayer.Character.Cone.Event
+
+while true do 
+
+
+Event:FireServer(
+    "Create",
+    CFrame.new(346.03103637695, 11.750509262085, -609.71063232422, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+)
+
+ task.wait(0.0001)
+ end
